2a9277a5f1923b2227c2dfaac205d12f4ecf0678
Erick Li <erickli42@utexas.edu>
2018-04-23 02:00:45 -0500

    added ADC convert function

--- ADC.h
+++ ADC.h
@@ -6,11 +6,27 @@
 #define ADC_H
 #include <stdint.h>
 
+
+// Player position structure
+typedef struct Player_Pos{
+    int16_t x;
+    int16_t y;
+} Player_Pos;
+
+
+// Move state enum 
+typedef enum move_state {LEFT, HOLD, RIGHT} MoveState;
+
+
+typedef uint8_t FSM; 
+    
+
 // ADC initialization function 
 // Input: none
 // Output: none
 // measures from PD2, analog channel 5
 void ADC_Init(void);
+
 
 //------------ADC_In------------
 // Busy-wait Analog to digital conversion
@@ -18,4 +34,12 @@ void ADC_Init(void);
 // Output: 12-bit result of ADC conversion
 // measures from PD2, analog channel 5
 uint32_t ADC_In(void);
+
+
+//------------ADC_Convert------------
+// Converts ADC_In to a position on the LCD
+// Input: none
+// Output: none
+MoveState ADC_Convert(void);
+
 #endif
